ce7be03a2d30d1dd20974616c1a316bb5b2f3818
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2020-03-22 03:27:46 +0100

    added scss too css plugin

--- src/client/index.js
+++ src/client/index.js
@@ -2,3 +2,8 @@ alert("i'm here");
 
 import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/header.scss';
+import './styles/form.scss';
